d5e87d2a7c3818d532a7f1b04ab99bc5c5a98e6f
truthNH <truth.nh@hotmail.co.jp>
2016-11-29 14:06:07 +0900

    initial commit

---
+++ Podfile
@@ -0,0 +1,12 @@
+# Uncomment this line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'WebSocket' do
+  # Uncomment this line if you're using Swift or would like to use dynamic frameworks
+  # use_frameworks!
+
+  pod 'SocketRocket'
+
+  # Pods for WebSocket
+
+end
--- WebSocket/ViewController.m
+++ WebSocket/ViewController.m
@@ -4,11 +4,13 @@
 //
 //  Created by natsuki.hirose on 2016/11/15.
 //  Copyright © 2016年 natsuki.hirose. All rights reserved.
-//
 
 #import "ViewController.h"
+#import "SRWebSocket.h"
 
-@interface ViewController ()
+@interface ViewController ()<SRWebSocketDelegate>
+
+@property(nonatomic, strong)SRWebSocket *web_socket;
 
 @end
 
@@ -17,11 +19,37 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    _web_socket = [[SRWebSocket alloc] initWithURLRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"ws://localhost:3000/"]]];
+    [_web_socket setDelegate:self];
+    [_web_socket open];
+    NSTimer *aTimer;
+    
+    aTimer = [NSTimer
+            timerWithTimeInterval:3
+            target:self
+              selector:@selector(sendToServer)
+            userInfo: nil repeats:YES];
+    
+    [[NSRunLoop mainRunLoop]
+        addTimer:aTimer
+        forMode:NSDefaultRunLoopMode];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)webSocketDidOpen:(SRWebSocket *)webSocket{
+    [webSocket send:@"123"];
+}
+
+- (void)webSocket:(SRWebSocket *)webSocket didReceiveMessage:(id)message {
+    NSLog(@"%@", [message description]);
+}
+
+- (void)sendToServer {
+    [_web_socket send:@"aaa"];
 }
 
 @end
